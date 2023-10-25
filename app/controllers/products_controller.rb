5d3a841c6e3afa3d898ee503c3b677dad726214a
askarf <ayalakarf@gmail.com>
2023-10-25 14:03:16 -0400

    added protection forgery exception, added create controller, added post route

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,11 +1,22 @@
 class ProductsController < ApplicationController
-  def all_products
+  def index
     @products = Product.all
     render :index
   end
 
-  def one_product
+  def show
     @product = Product.find_by(id: params["id"])
+    render :show
+  end
+
+  def create
+    @product = Product.new(
+      name: "Marc Jacobs Watch",
+      price: 75,
+      image_url: "www.tbd.com",
+      description: "slightly worn silver Marc by Marc jacobs watch.",
+    )
+    @product.save
     render :show
   end
 end
