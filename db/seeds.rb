7d5a0215ae4309baa9dd047f4154eaa806657518
askarf <ayalakarf@gmail.com>
2023-10-31 12:54:04 -0400

    updates seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,15 @@
-# Product.create!([
-#   { name: "Marc Jacobs Watch", price: "75.0", image_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQcdsWH386R9-dulSd0B9pTdULnppuw9kfcClC5n01mK0-fNTF_Yh1qpuJfjeQBQvj2ZBY&usqp=CAU", description: "slightly worn silver Marc by Marc jacobs watch.", stock: nil },
-#   { name: "Maroon Ted Baker Dress", price: "150.0", image_url: "https://di2ponv0v5otw.cloudfront.net/posts/2023/09/17/6507330fdcd5a5e41db99030/m_wp_65073316dcd5a5e41db99470.webp", description: "Maroon size 4 dress, velvet, midi, sleevless pleated with big bow.", stock: nil },
-#   { name: "Floral Doc Martins", price: "60.0", image_url: "https://di2ponv0v5otw.cloudfront.net/posts/2023/06/20/64926e89dff94d2fb1373ecb/m_wp_649c5331f644e56d54724c7c.webp", description: "Size 8 Doc Martin oxfords, navy and maroon with painted floral design, heavy wear but in decent condtion", stock: nil },
-#   { name: "Vans Sneakers", price: "50.0", image_url: "https://m.media-amazon.com/images/I/71iY3k7RZuS._AC_SR1840,1472_.jpg", description: "Platform Vans, black and white checkered sneakers. visable wear.", stock: nil },
-#   { name: "Replicate Mui Ballet Flats", price: "40.0", image_url: "https://img4.dhresource.com/webp/m/0x0/f3/albu/km/j/02/3c9be849-409e-4f96-a9f1-9706c3299c22.jpg", description: "Copy MuiMui pink ballet flats, size 7.5, with brown strap. never worn.", stock: nil },
-#   { name: "Sam Edelman Black Leather Hazel Pump", price: "80.0", image_url: "https://www.samedelman.com/blob/product-images/99900/ec/02/20419/ec0220419_pair_feed1000.jpg", description: "Slightly used, size 8 black pattened leather heels. 3.5 inch", stock: nil },
-#   { name: "White Floral Exquisite Gown", price: "900.0", image_url: "https://instagowns.com/wp-content/uploads/2022/06/IMG_2237-scaled-1.jpg", description: "Slightly used, size 4 great condition gown, long sleeve. White with embroidered colorful flowers.", stock: 1 },
+# Supplier.create!([
+#   { name: "Ayalas Closet", email: "ayalascloset@example.com", phone_number: "332-332-5455" },
+#   { name: "Mimi's leftovers", email: "hamydowns@example.com", phone_number: "112-321-5455" },
+#   { name: "straight from ali", email: "aligarbage@example.txt", phone_number: "1800-232-1222" },
 # ])
-# supplier = Supplier.new(name: "Ayalas Closet", email: "ayalascloset@example.com", phone_number: "332-332-5455")
-# supplier.save
-# supplier = Supplier.new(name: "Mimi's leftovers", email: "hamydowns@example.com", phone_number: "112-321-5455")
-# supplier.save
+# Product.create!([
+#   { name: "Maroon Ted Baker Dress", price: "150.0", image_url: "https://di2ponv0v5otw.cloudfront.net/posts/2023/09/17/6507330fdcd5a5e41db99030/m_wp_65073316dcd5a5e41db99470.webp", description: "Maroon size 4 dress, velvet, midi, sleevless pleated with big bow.", stock: 10, supplier_id: 3 },
+#   { name: "Pretty \nDress", price: "200.0", image_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQcdsWH386R9-dulSd0B9pTdULnppuw9kfcClC5n01mK0-fNTF_Yh1qpuJfjeQBQvj2ZBY&usqp=CAU", description: "sososoosoososoososo pretty:)", stock: 1, supplier_id: 3 },
+#   { name: "Marc Jacobs Watch", price: "75.0", image_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQcdsWH386R9-dulSd0B9pTdULnppuw9kfcClC5n01mK0-fNTF_Yh1qpuJfjeQBQvj2ZBY&usqp=CAU", description: "slightly worn silver Marc by Marc jacobs watch.", stock: 10, supplier_id: 2 },
+#   { name: "Floral Doc Martins", price: "60.0", image_url: "https://di2ponv0v5otw.cloudfront.net/posts/2023/06/20/64926e89dff94d2fb1373ecb/m_wp_649c5331f644e56d54724c7c.webp", description: "Size 8 Doc Martin oxfords, navy and maroon with painted floral design, heavy wear but in decent condtion", stock: 10, supplier_id: 2 },
+#   { name: "Vans Sneakers", price: "50.0", image_url: "https://m.media-amazon.com/images/I/71iY3k7RZuS._AC_SR1840,1472_.jpg", description: "Platform Vans, black and white checkered sneakers. visable wear.", stock: 10, supplier_id: 2 },
+#   { name: "Sam Edelman Black Leather Hazel Pump", price: "80.0", image_url: "https://www.samedelman.com/blob/product-images/99900/ec/02/20419/ec0220419_pair_feed1000.jpg", description: "Slightly used, size 8 black pattened leather heels. 3.5 inch", stock: 10, supplier_id: 2 },
+#   { name: "White Floral Exquisite Gown", price: "900.0", image_url: "https://instagowns.com/wp-content/uploads/2022/06/IMG_2237-scaled-1.jpg", description: "Slightly used, size 4 great condition gown, long sleeve. White with embroidered colorful flowers.", stock: 1, supplier_id: 2 },
+#   { name: "Replicate Mui Ballet Flats", price: "40.0", image_url: "https://img4.dhresource.com/webp/m/0x0/f3/albu/km/j/02/3c9be849-409e-4f96-a9f1-9706c3299c22.jpg", description: "Copy MuiMui pink ballet flats, size 7.5, with brown strap. never worn.", stock: 10, supplier_id: 4 },
+# ])
